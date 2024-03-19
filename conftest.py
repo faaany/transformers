2c31703177ca56c8ea859a9b1bc048b7173c41a1
Lin, Fanli <fanli.lin@intel.com>
2024-03-19 11:16:44 -0700

    add sagemaker

--- conftest.py
+++ conftest.py
@@ -72,6 +72,7 @@ NOT_DEVICE_TESTS = {
     "/repo_utils/",
     "/utils/",
     "/tools/",
+    "/sagemaker/",
 }
 
 # allow having multiple repository checkouts and not needing to remember to rerun
