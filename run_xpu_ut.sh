72bf6e35d03c3449a026745533c6ba8abc4ccae6
Lin, Fanli <fanli.lin@intel.com>
2024-03-19 11:29:11 -0700

    fix

--- run_xpu_ut.sh
+++ run_xpu_ut.sh
@@ -4,6 +4,8 @@ echo "+++++++++remove excel dir if exists and create a new++++++++++++"
 rm -fr $excel_dir 
 mkdir $excel_dir
  
+mv tests/models . 
+
 echo "+++++++++run test file++++++++++++++++"
 pytest tests/*.py -rA -s -n auto --dist=loadfile -m "not (not_device_test or require_torch_up_to_2_gpus or require_torch_multi_gpu or require_torch_gpu or require_torch_non_multi_gpu or require_torch_bf16_gpu or require_torch_tf32 or require_torch_npu or require_torch_multi_npu or require_torch_xla or require_torch_tensorrt_fx or require_apex or is_pt_flax_cross_test or is_pt_tf_cross_test or require_tf or require_flax or require_torch_neuroncore or require_natten)" -k "not (tpu or cuda or npu or flax or tf)" --excelreport="${excel_dir}/single_files.xlsx" --make-reports="single_files" --timeout=600
 echo "+++++++++++++++++++++++done for single_files+++++++++++++"
