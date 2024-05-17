4ed69ddc3447a7edee4b34cf286a66c03784d295
Lin, Fanli <fanli.lin@intel.com>
2024-05-17 03:03:13 -0700

    fix model_parellism

--- src/transformers/models/m2m_100/modeling_m2m_100.py
+++ src/transformers/models/m2m_100/modeling_m2m_100.py
@@ -519,7 +519,7 @@ class M2M100PreTrainedModel(PreTrainedModel):
     config_class = M2M100Config
     base_model_prefix = "model"
     supports_gradient_checkpointing = True
-    _no_split_modules = ["M2M100Attention"]
+    _no_split_modules = ["M2M100Attention", "M2M100EncoderLayer", "M2M100DecoderLayer"]
 
     def _init_weights(self, module):
         std = self.config.init_std
